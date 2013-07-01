a46f4a14c4578ab8c0a10a7f88b7549db8560793
mhdrz <reza.saberi@afghan-wireless.com>
2013-07-01 10:45:06 +0430

    Create lbexa.php

---
+++ application/views/exams/lbexa.php
@@ -0,0 +1,6 @@
+<a href="<?php echo base_url().'index.php/exams/home';?>" >Home</a><br>
+<a href="<?php echo base_url().'index.php/exams/active_exams';?>" >Active exams</a><br>
+<a href="<?php echo base_url().'index.php/exams/finished_exams';?>" >Finished exams</a><br>
+<a href="<?php echo base_url().'index.php/exams/dismissed_exams';?>" >Dismissed exams</a><br>
+<a href="<?php echo base_url().'index.php/exams/suspended_exams';?>" >Suspended exams</a><br>
+<a href="<?php echo base_url().'index.php/exams/new_exam';?>" >New exam</a><br>
