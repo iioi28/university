352670ae24b8280f17d42ea662840cce1324876b
mhdrz <reza.saberi@afghan-wireless.com>
2013-07-01 10:51:28 +0430

    Create main_view_sub.php

---
+++ application/views/subjects/main_view_sub.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+		<title> Main View </title>
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url().'css/main_style.css';?>" />
+		<script type="text/javascript" src="<?php echo base_url().'js/jquery.js';?>" ></script>
+	</head>
+	<body>
+		
+		<div id="all-page">
+			<div id="header">
+			
+			</div>
+			<!--
+			<div id="main-menu">
+			</div>
+			-->
+			<div id="left-bar">
+				<?php $this->load->view('lbsub'); ?>
+			</div>
+			<div id="content">
+					<?php $this->load->view($page); ?>
+			</div>
+		</div>
+	
+	</body>
+</html>
