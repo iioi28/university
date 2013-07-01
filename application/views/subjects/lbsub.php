a0d9cef12e1206d4968930650e33b40baee8e8c6
mhdrz <reza.saberi@afghan-wireless.com>
2013-07-01 10:47:46 +0430

    Create lbsub.php

---
+++ application/views/subjects/lbsub.php
@@ -0,0 +1,3 @@
+<a href="<?php echo base_url().'index.php/subjects/home';?>" >Home</a><br>
+<a href="<?php echo base_url().'index.php/subjects/all_subjects';?>" >All Subjects</a><br>
+<a href="<?php echo base_url().'index.php/subjects/new_subject';?>" >New Subjects</a><br>
