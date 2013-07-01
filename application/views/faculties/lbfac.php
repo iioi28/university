4e27c85027167f26e2bcf31097d8d8cea7ece732
mhdrz <reza.saberi@afghan-wireless.com>
2013-07-01 10:43:53 +0430

    Create lbfac.php

---
+++ application/views/faculties/lbfac.php
@@ -0,0 +1,3 @@
+<a href="<?php echo base_url().'index.php/faculties/home';?>" >Home</a><br>
+<a href="<?php echo base_url().'index.php/faculties/all_faculties';?>" >All Faculties</a><br>
+<a href="<?php echo base_url().'index.php/faculties/new_faculty';?>" >New Faculty</a><br>
