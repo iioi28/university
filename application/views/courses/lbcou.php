9d0c578fc0d08eb6bb2bde8947f49be5a575be71
mhdrz <reza.saberi@afghan-wireless.com>
2013-07-01 10:39:38 +0430

    Create lbcou.php

---
+++ application/views/courses/lbcou.php
@@ -0,0 +1,6 @@
+<a href="<?php echo base_url().'index.php/courses/home';?>" >Home</a><br>
+<a href="<?php echo base_url().'index.php/courses/active_courses';?>" >Active Courses</a><br>
+<a href="<?php echo base_url().'index.php/courses/finished_courses';?>" >Finished Courses</a><br>
+<a href="<?php echo base_url().'index.php/courses/dismissed_courses';?>" >Dismissed Courses</a><br>
+<a href="<?php echo base_url().'index.php/courses/suspended_courses';?>" >Suspended Courses</a><br>
+<a href="<?php echo base_url().'index.php/courses/new_course';?>" >New Course</a><br>
