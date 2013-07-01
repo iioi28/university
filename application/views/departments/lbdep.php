c849c9fb5bc939896ef3bb606c99289b22428b1f
mhdrz <reza.saberi@afghan-wireless.com>
2013-07-01 10:42:24 +0430

    Create lbdep.php

---
+++ application/views/departments/lbdep.php
@@ -0,0 +1,3 @@
+<a href="<?php echo base_url().'index.php/departments/home';?>" >Home</a><br>
+<a href="<?php echo base_url().'index.php/departments/all_departments';?>" >All Departments</a><br>
+<a href="<?php echo base_url().'index.php/departments/new_department';?>" >New Department</a><br>
