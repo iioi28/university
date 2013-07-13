2a1ee7ab7f48357fd5f80e5d3e60a88bccce3d09
iioi28 <alihussainpanahi@aol.com>
2013-07-13 13:35:19 +0430

    Create left.php

---
+++ application/views/main/left.php
@@ -0,0 +1,5 @@
+<div id="left-bar">
+<a href="<?php echo base_url();?>" >Home</a><br>
+<a href="<?php echo base_url().'faculties/all_faculties';?>" >All Faculties</a><br>
+<a href="<?php echo base_url().'faculties/new_faculty';?>" >New Faculty</a><br>
+</div>
