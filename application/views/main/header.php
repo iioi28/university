971f80ec3818d351f810a49849b53010a56a8865
iioi28 <alihussainpanahi@aol.com>
2013-07-13 13:34:58 +0430

    Create header.php

---
+++ application/views/main/header.php
@@ -0,0 +1,10 @@
+<head>
+  <link rel="stylesheet" href="<?=base_url()?>css/layout.css" type="text/css" />
+	<link rel="stylesheet" href="<?=base_url()?>css/style.css" type="text/css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'css/main_style.css';?>" />
+	<script type="text/javascript" src="<?=base_url()?>js/core.js"></script>
+	<script type="text/javascript" src="<?=base_url()?>js/ajax_core.js"></script>
+</head>
+<div id="header">
+	<h1 align="center"> select an item </h1>
+</div>
